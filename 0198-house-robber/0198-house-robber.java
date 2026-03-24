e174652db8fcb976fd3d440e8a55e1d274716432
Manish Sorout <154682588+ManishSorout07@users.noreply.github.com>
2026-03-24 09:44:17 +0530

    Time: 0 ms (100%), Space: 42.5 MB (92.15%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n==0){
+           return 0;
+        }
+
+        if(n==1){
+            return nums[0];
+        }
+
+        int prev2 = nums[0];
+        int prev = Math.max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++){
+            int curr = Math.max(prev, prev2+nums[i]);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
