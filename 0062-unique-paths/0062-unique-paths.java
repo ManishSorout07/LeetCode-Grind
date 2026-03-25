fb8fecf893fc8ea8bea8d23d3578eda8affa9efa
Manish Sorout <154682588+ManishSorout07@users.noreply.github.com>
2026-03-25 11:12:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.08%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int paths(int i, int j, int[][]dp ){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(i==0 && j==0)return 1;
+
+        if(dp[i][j]!= -1)return dp[i][j];
+
+        int left = paths(i,j-1,dp);
+        int top = paths(i-1,j,dp);
+
+        return dp[i][j] = top+left;
+    
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for(int i=0; i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
+            }
+        } 
+       return paths(m-1,n-1,dp);
+    }
+}
