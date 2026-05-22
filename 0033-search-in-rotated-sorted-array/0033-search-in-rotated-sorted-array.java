3616fc5cf9c6413a3771243c7487b4ed15ba2c4a
Manish Sorout <154682588+ManishSorout07@users.noreply.github.com>
2026-05-22 22:52:28 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.61%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start =0;
+        int end = nums.length-1;
+
+        int pivot = pivotElement(nums);
+        if(pivot == -1){
+            return binarySearch(nums,target,start,end);
+        }
+        if(nums[pivot]==target){
+            return pivot;
+        }
+
+        if(target >= nums[0]){
+            return binarySearch(nums,target,start,pivot-1);
+        }
+        
+    return binarySearch(nums,target,pivot+1,end);
+    }
+    
+    public int binarySearch(int[]nums, int target, int start, int end){
+        
+        while(start<=end){
+            int mid = start+(end-start)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] > target){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+        }
+        return -1;
+    }
+
+    public int pivotElement(int[]nums){
+        int start =0;
+        int end = nums.length-1;
+
+        while(start<=end){
+        int mid = start+(end-start)/2;
+
+        if(mid<end && nums[mid]>nums[mid+1]){
+            return mid;
+        }
+        if(mid>start && nums[mid] < nums[mid-1]){
+            return mid-1;
+        }
+        if(nums[start] >= nums[mid]){
+          end = mid-1;
+        }
+        if(nums[start] < nums[mid]){
+            start = mid+1;
+        }
+        
+    }
+    return -1;
+    }
+
+
+}
