fe32dada6edcdaf45f6143957bedc27ae3786094
Manish Sorout <154682588+ManishSorout07@users.noreply.github.com>
2026-09-19 23:36:38 +0530

    Time: 1 ms (99.86%), Space: 42.8 MB (95.55%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,33 +1,25 @@
-import java.util.HashMap;
-
 class Solution {
     public int longestPalindrome(String s) {
+        
+        int freq[] = new int[128];
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     freq[s.charAt(i)]++;
+        // }
 
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        for(int ch: s.toCharArray()){
+            freq[ch]++;
         }
 
-        int count = 0;
-        boolean odd = false;
-
-        for (int freq : map.values()) {
-
-            count += (freq / 2) * 2;
-
-            if (freq % 2 == 1) {
-                odd = true;
+        int res=0;
+        for(int i=0; i<128;i++){
+            int val=freq[i];
+            res+=(val/2)*2;
+            
+            if(res%2==0 && val%2==1){
+                res++;
             }
         }
-
-        if (odd) {
-            count++;
-        }
-
-        return count;
+        return res;
     }
 }
